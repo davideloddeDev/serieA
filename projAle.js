0754c198339db100591226b38bd835fccab402e4
Alex <al.vaccaro@outlook.com>
2023-11-30 11:48:34 +0100

    Creato oggetto dentro file projAle.js

--- projAle.js
+++ projAle.js
@@ -1,1 +1,12 @@
-let array = []
+let alessandro = {
+    name: "Alessandro",
+    surname: "Vaccaro",
+    age: 31,
+    city: "Pisa",
+    hobby: "Wrestling, musica , videogames",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "World Of Warcraft",
+    favoriteFilm: "Il Miglio Verde",
+    favoriteBook: "Il Ciclo dell'Odio",
+    petName: "Giorgio"
+    };
