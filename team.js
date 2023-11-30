5da8b8cd290b4aeef2606dc49e4396c688e4189b
martinamancuso <martinamancuso24@gmail.com>
2023-11-30 12:00:51 +0100

    first commit

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-let array = []
+let array = [{ 
+    name: "Martina",
+    surname: "Mancuso",
+    age: 32,
+    city: "Milano",
+    hobby: "reading",
+    favoriteFood: "indian food",
+    favoriteVideoGame: "tomb rider",
+    favoriteFilm: "mamma mia",
+    favoriteBook: "solo bagaglio a mano",
+    petName: "Neve"
+  }];
